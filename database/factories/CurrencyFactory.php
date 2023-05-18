db5812b02938f31e05678cc823ed6bb001acc7d5
hernan arica <hernanaricammm@gmail.com>
2023-05-18 16:22:09 -0300

    se agrega el seeder y factory para currency

---
+++ database/factories/CurrencyFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Currency>
+ */
+class CurrencyFactory extends Factory
+{
+  /**
+   * Define the model's default state.
+   *
+   * @return array<string, mixed>
+   */
+  public function definition(): array
+  {
+    return [
+      'value'      => $this->faker->randomFloat(2, 100, 400),
+      'country_id' => $this->faker->numberBetween(1, 20),
+      'created_at' => date('Y-m-d H:i:s'),
+      'updated_at' => date('Y-m-d H:i:s')
+    ];
+  }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,6 +18,7 @@ class DatabaseSeeder extends Seeder
       CountrySeeder::class,
       EntitySeeder::class,
       CompanySeeder::class,
+      CurrencySeeder::class,
     ]);
   }
 }
