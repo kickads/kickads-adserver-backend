06437127857e55327be7041670b7e6afeae48808
hernan arica <hernanaricammm@gmail.com>
2023-05-18 15:31:52 -0300

    se agrega el seeder y factory de countries

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,7 +14,8 @@ class DatabaseSeeder extends Seeder
     $this->call([
       RoleSeeder::class,
       UserSeeder::class,
-      BusinessModelSeeder::class
+      BusinessModelSeeder::class,
+      CountrySeeder::class
     ]);
   }
 }
