c0aa787dfd6394cc6fa84d399571044dae539091
hernan arica <hernanaricammm@gmail.com>
2023-05-19 20:55:48 -0300

    se relevan los campos necesarios para la creacion de un deal

--- database/migrations/2023_05_18_131426_create_deals_table.php
+++ database/migrations/2023_05_18_131426_create_deals_table.php
@@ -12,17 +12,23 @@ return new class extends Migration {
   {
     Schema::create('deals', function (Blueprint $table) {
       $table->id();
+      /*
+       * TODO: agregar
+       * executive_id
+       * ios_id
+       * campaigns_category_id
+       * priority_id
+       */
       $table->foreignId('business_model_id')->constrained();
       $table->foreignId('advertiser_id')->constrained();
       $table->foreignId('entity_id')->constrained();
       $table->foreignId('company_id')->constrained();
-      $table->string('agency_fee');
-      $table->string('agency');
       $table->string('goal_description');
       $table->string('start_date');
       $table->string('end_date');
       $table->string('open_budget');
-      $table->string('available_budget');
+      $table->string('agency_commission');
+      $table->string('budget_total');
       $table->string('goals_units');
       $table->string('exchange_rate');
       $table->string('campaign_name');
